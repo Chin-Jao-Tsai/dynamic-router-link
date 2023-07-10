c18701b83ca46e23fec77940d80acae66eaadaa5
Chin-Jao-Tsai <135097802+Chin-Jao-Tsai@users.noreply.github.com>
2023-07-10 16:51:15 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Location } from "@angular/common";
 import { Component, VERSION } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { GreetingService } from "./service/greeting.service";
 
 @Component({
   selector: "my-app",
@@ -13,13 +14,18 @@ export class AppComponent {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router, 
-    private readonly location: Location
+    private readonly location: Location,
+    public gs: GreetingService
   ) {}
 
 
 
   changeParam(value: string): void {
     // this.router.navigate([], { queryParams: { param: value } });
-    this.location.back();
+    // this.location.back();
+  }
+
+  childComEmit(event: any){
+    alert(event);
   }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h1 (click)='click()'>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
   @Input() name: string;
+  @Output() childEvent = new EventEmitter();
+
+  click(){
+    this.childEvent.emit("hello Component");
+  }
 }
